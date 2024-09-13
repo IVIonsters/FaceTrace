1a4b254951ea97437244999631e627f9d358498d
Zachary Polof <ZacharyPolof@gmail.com>
2024-09-12 22:33:14 -0500

    Particles
    adjusted particles on background

--- src/App.js
+++ src/App.js
@@ -2,13 +2,26 @@ import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageForm from './components/ImageForm/ImageForm';
+import Rank from './components/Rank/Rank';
 import 'tachyons';
+import ParticlesBg from 'particles-bg'
+
+const particlesParams = {
+  className: 'particles',
+  type: 'cobweb',
+  bg: true,
+  num: 800,
+  color: '#ffffff',
+}
+
 
 function App() {
   return (
     <div className="App">
+      <ParticlesBg {...particlesParams} />
       <Navigation />
       <Logo />
+      <Rank />
       <ImageForm />
     </div>
   );
---
+++ src/components/Rank/Rank.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Rank = () => {
+  return (
+    <div>
+      <div className='navy f3'>
+        {'IVIonsters, your current rank is...'}
+      </div>
+      <div className='navy f1'>
+        {'#3'}
+      </div>
+    </div>
+  );
+}
+
+export default Rank;
