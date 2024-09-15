5547327a050fff9f9a5325f64f146c61e1863158
Zachary Polof <ZacharyPolof@gmail.com>
2024-09-15 10:01:00 -0500

    Clarifai
    Clarifai API setup and integration

--- src/App.js
+++ src/App.js
@@ -1,36 +1,103 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageForm from './components/ImageForm/ImageForm';
 import Rank from './components/Rank/Rank';
 import 'tachyons';
-import ParticlesBg from 'particles-bg'
+import ParticlesBg from 'particles-bg';
 
+// Your PAT (Personal Access Token) can be found in the Account's Security section
+const PAT = 'f91116724bf84f14ac7e05de6b2af897';
+// Specify the correct user_id/app_id pairings
+// Since you're making inferences outside your app's scope
+const USER_ID = 'clarifai';
+const APP_ID = 'main';
+// Change these to whatever model and image URL you want to use
+const MODEL_ID = 'face-detection';
+const MODEL_VERSION_ID = '6dc7e46bc9124c5c8824be4822abe105';
+const IMAGE_URL = 'https://samples.clarifai.com/metro-north.jpg';
+
+// Clarifai API request options
+const raw = JSON.stringify({
+  "user_app_id": {
+      "user_id": USER_ID,
+      "app_id": APP_ID
+  },
+  "inputs": [
+      {
+          "data": {
+              "image": {
+                  "url": IMAGE_URL
+                  // "base64": IMAGE_BYTES_STRING
+              }
+          }
+      }
+  ]
+});
+
+const requestOptions = {
+  method: 'POST',
+  headers: {
+      'Accept': 'application/json',
+      'Authorization': 'Key ' + PAT
+  },
+  body: raw
+};
+
+// Params for particles-bg
 const particlesParams = {
   className: 'particles',
   type: 'cobweb',
   bg: true,
   num: 700,
   color: '#ffffff',
-}
-
+};
 
 class App extends Component {
+  // constructor for the state
   constructor() {
     super();
     this.state = {
       input: '',
-    }
+    };
   }
 
+  // Handles input on Image form - url
   onInputChange = (event) => {
     console.log(event.target.value);
-  }
+    this.setState({ input: event.target.value });
+  };
 
+  // handles the "detect" button on ImageForm
   onButtonSubmit = (event) => {
-    console.log('click');
-  }
+    fetch("https://api.clarifai.com/v2/models/" + MODEL_ID + "/versions/" + MODEL_VERSION_ID + "/outputs", requestOptions)
+    .then(response => response.json())
+    .then(result => {
+
+        const regions = result.outputs[0].data.regions;
+
+        regions.forEach(region => {
+            // Accessing and rounding the bounding box values
+            const boundingBox = region.region_info.bounding_box;
+            const topRow = boundingBox.top_row.toFixed(3);
+            const leftCol = boundingBox.left_col.toFixed(3);
+            const bottomRow = boundingBox.bottom_row.toFixed(3);
+            const rightCol = boundingBox.right_col.toFixed(3);
+
+            region.data.concepts.forEach(concept => {
+                // Accessing and rounding the concept value
+                const name = concept.name;
+                const value = concept.value.toFixed(4);
+
+                console.log(`${name}: ${value} BBox: ${topRow}, ${leftCol}, ${bottomRow}, ${rightCol}`);
+                
+            });
+        });
+
+    })
+    .catch(error => console.log('error', error));
+  };
 
   render() {
     return (
