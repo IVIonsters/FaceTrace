50d5e8390587c9e73953516c94d42b2e42712561
Zachary Polof <ZacharyPolof@gmail.com>
2024-09-15 09:22:10 -0500

    Function changes
    added function for input change and button submit listeners and triggered on ImageForm

--- src/App.js
+++ src/App.js
@@ -16,16 +16,35 @@ const particlesParams = {
 }
 
 
-function App() {
-  return (
-    <div className="App">
-      <ParticlesBg {...particlesParams} />
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageForm />
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  }
+
+  onButtonSubmit = (event) => {
+    console.log('click');
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <ParticlesBg {...particlesParams} />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageForm 
+          onInputChange={this.onInputChange} 
+          onButtonSubmit={this.onButtonSubmit} />
+      </div>
+    );
+  }
 }
 
 export default App;
