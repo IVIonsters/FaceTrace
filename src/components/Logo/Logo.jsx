3e06c5b957825cc5c0a776049ee6945a8c02065b
Zachary Polof <ZacharyPolof@gmail.com>
2024-09-15 22:14:39 -0500

    Logo
    adjusted logo position

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -5,8 +5,8 @@ import './Logo.css';
 
 const Logo = () => {
   return (
-    <div className='ma4 mt0'>
-      <Tilt>
+    <div className='ma4 mt0 logo-container'>
+      <Tilt tiltMaxAngleX={10} tiltMaxAngleY={10}>
         <div className='Tilt pa3' style={{ height: '150px', width: '150px' }}>
           <img style={{paddingTop: '5px'}} 
           alt='logo' 
