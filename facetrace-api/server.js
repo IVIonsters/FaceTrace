91983d4b2c73aa6c5702dedebd8edf68a60c948e
Zachary Polof <ZacharyPolof@gmail.com>
2024-09-22 22:03:06 -0500

    Update server.js
    setup basic test route
    
    added comments for future routes needed

--- facetrace-api/server.js
+++ facetrace-api/server.js
@@ -1,7 +1,22 @@
+// setup express server
 const express = require('express');
-
 const app = express();
 
+// Routes
+app.get('/', (req, res) => {
+    res.send('Ello Mate, Shes working! 👋');
+})
+
+// Port
 app.listen(3000, () => {
     console.log('Server, Shes up and running on port 3000 mate! 🚀');
 })
+
+
+/* 
+routes --> res = this is working
+signin --> POST = success/fail
+register --> POST = user
+profile/:userId --> GET = user
+image --> PUT --> user
+*/
