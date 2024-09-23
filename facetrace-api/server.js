2110e6df2aefeca4a674539a6b94985c217e871d
Zachary Polof <ZacharyPolof@gmail.com>
2024-09-22 22:52:44 -0500

    Update server.js
    updated image route, tested with postman and confirmed entries changed for each user

--- facetrace-api/server.js
+++ facetrace-api/server.js
@@ -74,6 +74,20 @@ app.get('/profile/:id', (req, res)=> {
 });
 
 // Image
+app.put('/image', (req, res) => {
+  const { id } = req.body;
+  let found = false;
+  database.users.forEach(user => {
+    if (user.id === id) {
+      found = true;
+      user.entries++
+      return res.json(user.entries);
+    } 
+  })
+  if (!found) {
+    res.status(400).json('Sorry Mate, No user found!');
+  }
+});
 
 // Port
 app.listen(3000, () => {
