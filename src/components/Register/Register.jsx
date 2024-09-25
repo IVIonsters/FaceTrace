67ae7f7fb0c3e8ce6c59c73e83b3d28cc06b01ac
Zachary Polof <ZacharyPolof@gmail.com>
2024-09-24 21:37:34 -0500

    Update Register.jsx
    update Register for registration ability.
    
    modified component with new props and functions to handle registration

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,41 +1,110 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Register = ({ onRouteChange }) => {
-  const handleSubmit = (event) => {
+class Register extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      email: '',
+      password: ''
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.onNameChange = this.onNameChange.bind(this);
+    this.onEmailChange = this.onEmailChange.bind(this);
+    this.onPasswordChange = this.onPasswordChange.bind(this);
+  }
+
+  // Update the state with the value of the input field - name
+  onNameChange(event) {
+    this.setState({ name: event.target.value });
+  }
+
+  // Update the state with the value of the input field - email
+  onEmailChange(event) {
+    this.setState({ email: event.target.value });
+  }
+
+  // Update the state with the value of the input field - password
+  onPasswordChange(event) {
+    this.setState({ password: event.target.value });
+  }
+
+  // Handles the submit button on the form
+  handleSubmit(event) {
     event.preventDefault();
-    onRouteChange('home');
-  };
+    fetch("http://localhost:3001/register", {
+      method: "post",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        name: this.state.name,
+        email: this.state.email,
+        password: this.state.password,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.id) {
+          this.props.onRouteChange("home");
+        } else {
+          // Handle registration failure (e.g., show an error message)
+          console.error("Registration failed");
+        }
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }
 
-  return (
-    <article className='br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center'>
-      <main className="pa4 black-80">
-        <form className="measure" onSubmit={handleSubmit}>
-          <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-            <legend className="f1 fw6 ph0 mh0">Register</legend>
-            <div className="mt3">
-              <label className="db fw6 lh-copy f6" htmlFor="name">Name</label>
-              <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="text" name="name" id="name" />
+  render() {
+    return (
+      <article className='br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center'>
+        <main className="pa4 black-80">
+          <form className="measure" onSubmit={this.handleSubmit}>
+            <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
+              <legend className="f1 fw6 ph0 mh0">Register</legend>
+              <div className="mt3">
+                <label className="db fw6 lh-copy f6" htmlFor="name">Name</label>
+                <input 
+                  className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
+                  type="text" 
+                  name="name" 
+                  id="name" 
+                  onChange={this.onNameChange}
+                />
+              </div>
+              <div className="mt3">
+                <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
+                <input 
+                  className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
+                  type="email" 
+                  name="email-address" 
+                  id="email-address" 
+                  onChange={this.onEmailChange}
+                />
+              </div>
+              <div className="mv3">
+                <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
+                <input 
+                  className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
+                  type="password" 
+                  name="password" 
+                  id="password" 
+                  onChange={this.onPasswordChange}
+                />
+              </div>
+            </fieldset>
+            <div className="">
+              <input 
+                className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" 
+                type="submit" 
+                value="Register" 
+              />
             </div>
-            <div className="mt3">
-              <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
-              <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address" id="email-address" />
-            </div>
-            <div className="mv3">
-              <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
-              <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password" id="password" />
-            </div>
-          </fieldset>
-          <div className="">
-            <input 
-              className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" 
-              type="submit" 
-              value="Register" 
-            />
-          </div>
-        </form>
-      </main>
-    </article>
-  );
-};
+          </form>
+        </main>
+      </article>
+    );
+  }
+}
 
 export default Register;
